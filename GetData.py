1f21a1814a14f89436c36c933aae63c641f38b36
Tree-Sooyeon-Yim <yimsooyeon9@gmail.com>
2021-05-23 15:30:39 +0900

    added save

--- GetData.py
+++ GetData.py
@@ -10,53 +10,73 @@ Name:GetData
 --------------------------------------
 """
 
+
 import csv
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
+import time
+
 def book_crawling():
-    driver=webdriver.Chrome(ChromeDriverManager().install())
-    driver.implicitly_wait(30)
-    driver.get(url)
-    bsObject=BeautifulSoup(driver.page_source,'html.parser')
-    book_page_urls=[]
-    for index in range(0,20):
-        dl_data=bsObject.find('dt',{'id':"book_title_"+str(index)})
-        link=dl_data.select('a')[0].get('href')
-        book_page_urls.append(link)
-        
-    for index,book_page_url in enumerate(book_page_urls):   
-        driver.get(book_page_url)
-        bsObject=BeautifulSoup(driver.page_source,'html.parser')
-        
-        dl_title=bsObject.find('div',{'class':'book_info'})
-        title=dl_title.select('a')[0].get_text()
-        url=bsObject.find('div',{'id':'bookIntroContent'}).get_text()
-        
-        book_info={'index':index,'title':title,'abstract':url}
-    
-        book_info_list.append(book_info)
-    return book_info_list
+        for page in range(1,6):
+            print(page)
+            url=move_page(page)
+            print(url)
+            driver=webdriver.Chrome(ChromeDriverManager().install())#웹드라이버 정의
+            driver.implicitly_wait(30) #웹페이지 파싱될때까지 최대 30초 기다림.
+            driver.get(url)
+            bsObject=BeautifulSoup(driver.page_source,'html.parser')
+            book_page_urls=[]
+            for index in range(0,20):
+                dl_data=bsObject.find('dt',{'id':"book_title_"+str(index)})
+                link=dl_data.select('a')[0].get('href')
+                book_page_urls.append(link)
+                
+                
+            for index,book_page_url in enumerate(book_page_urls):   
+                driver.get(book_page_url)
+                bsObject=BeautifulSoup(driver.page_source,'html.parser')
+            
+                dl_title=bsObject.find('div',{'class':'book_info'})
+                title=dl_title.select('a')[0].get_text()
+                url=bsObject.find('div',{'id':'bookIntroContent'}).get_text()
+                print(title)
+                print(url)
+                book_info.append([title,url])
+                #book_info={title:url}
+                #print(book_info[0])
+                #print(book_info[1])
+                #book_info_list.append(book_info)
+        return book_info
 
-def save_data(book_info):
-    keys=book_info[0].keys()
-    with open('book_crawling.csv','w')as file:
-        writer=csv.DictWriter(file,keys)
-        writer.writeheader()
-        writer.writerows(book_info)
- 
+def save_data():
+        csvFile=open("C:/Users/yimso/Downloads/etc/novel1.csv",'wt',encoding='utf-8-sig',newline='')#a옵션...
+        writer=csv.writer(csvFile)
+        columns=['title','abstract']
+        writer.writerow(columns)
+        try:
+            #for i in enumerate(book_info):
+            writer.writerows(book_info)
+        finally:
+            csvFile.close
+
+
 def move_page(page):
-    path=r'http://book.naver.com/category/index.nhn?cate_code=100010010&tab=top100&list_type=list&sort_type=publishday&page='+page
-    return path
+        path='http://book.naver.com/category/index.nhn?cate_code=330090&tab=top100&list_type=list&sort_type=publishday&page='+str(page)
+        return path
 
 
-book_info_list=[]
-for page in range(1,6):
-    url=move_page(page)
-    book_crawling()
-    
-save_data(book_info_list)  
+
+book_info=[]
+try:
+        book_crawling()
+except:
+        save_data()    
+        
+
+
+
 
 
     
